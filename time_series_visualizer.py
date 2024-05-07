5296d6c3ed404d3a89a3e40ac235c570374fcd9a
Zhang Beiduo <1801764634@qq.com>
2024-05-07 06:54:34 +0000

    feat: init gitpod config, replace poetry with pip (#16) * feat: init gitpod config, replace poetry with pip install via requirements file
    * fix: use main.py as the entrypoint
    
    * Update .gitpod.yml
    
    Co-authored-by: Shaun Hamilton <shauhami020@gmail.com>
    
    ---------
    
    Co-authored-by: Shaun Hamilton <shauhami020@gmail.com>
    
    Checklist:
    
    <!-- Please follow this checklist and put an x in each of the boxes, like this: [x]. It will ensure that our team takes your pull request seriously. -->
    
    - [ ] I have read [freeCodeCamp's contribution guidelines](https://contribute.freecodecamp.org).
    - [ ] My pull request has a [descriptive title](https://contribute.freecodecamp.org/#/how-to-open-a-pull-request?id=prepare-a-good-pr-title) (**not** a vague title like `Update index.md`)
    
    <!--If your pull request closes a GitHub issue, replace the XXXXX below with the issue number.-->
    
    Closes #XXXXX
    
    <!-- Feel free to add any additional description of changes below this line -->

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,36 +5,50 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv('datas/fcc-forum-pageviews.csv', 
+                 parse_dates=['date'], 
+                 index_col=['date'])
 
 # Clean data
-df = None
+df = df[
+    (df['value'] >= df['value'].quantile(0.025))&
+    (df['value'] <= df['value'].quantile(0.975))
+]
 
 
 def draw_line_plot():
     # Draw line plot
+    plt.figure(figsize=(15, 5))
+    plt.plot(df['value'])
 
-
-
-
+    plt.title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    plt.xlabel('Date')
+    plt.ylabel('Page Views')
 
     # Save image and return fig (don't change this part)
-    fig.savefig('line_plot.png')
-    return fig
+    plt.savefig('line_plot.png')
+    return plt
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    ddf_bar = df.copy()
+    df_bar.reset_index(inplace=True)
+    df_bar['year'] = df_bar['date'].dt.year
+    df_bar['month'] = df_bar['date'].dt.strftime('%B')
+    df_bar = df_bar.groupby(['year', 'month'], as_index=False)['value'].mean()
 
     # Draw bar plot
-
-
-
-
+    plt.figure(figsize=(8, 8))
+    sns.barplot(x='year', y='value', hue='month', 
+                data=df_bar, palette='tab20')
+    
+    plt.xlabel('Years')
+    plt.ylabel('Average Page Views')
+    plt.legend(title='Months', loc='upper left')
 
     # Save image and return fig (don't change this part)
-    fig.savefig('bar_plot.png')
-    return fig
+    plt.savefig('bar_plot.png')
+    return plt
 
 def draw_box_plot():
     # Prepare data for box plots (this part is done!)
@@ -44,11 +58,20 @@ def draw_box_plot():
     df_box['month'] = [d.strftime('%b') for d in df_box.date]
 
     # Draw box plots (using Seaborn)
-
-
-
-
+    plt.figure(figsize=(30, 10))
+    
+    plt.subplot(1, 2, 1)
+    sns.boxplot(x='year', y='value', data=df_box, fill=False)
+    plt.title('Year-wise Box Plot (Trend)')
+    plt.xlabel('Year')
+    plt.ylabel('Page Views')
+    
+    plt.subplot(1, 2, 2)
+    sns.boxplot(x='month', y='value', data=df_box)
+    plt.title('Month-wise Box Plot (Seasonality)')
+    plt.xlabel('Month')
+    plt.ylabel('Page Views')
 
     # Save image and return fig (don't change this part)
-    fig.savefig('box_plot.png')
-    return fig
+    plt.savefig('box_plot.png')
+    return plt
