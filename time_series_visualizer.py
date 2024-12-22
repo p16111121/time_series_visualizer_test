8bd72b1859499071f21a9a6c9b1f12df41f78434
p16111121 Bo <p16111121@gs.ncku.edu.tw>
2024-12-22 18:48:45 +0000

    加上註解

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,6 +5,7 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
+# 讀取 CSV 文件並指定 'date' 列為日期格式（這樣 pandas 會自動將其解析為 datetime 格式）
 df = pd.read_csv('fcc-forum-pageviews.csv',parse_dates=['date'])
 df.set_index('date', inplace=True)
 
@@ -14,13 +15,13 @@ df = df[(df['value'] >= df['value'].quantile(0.025)) & (df['value'] <= df['value
 
 def draw_line_plot():
     # Draw line plot
-    fig, ax = plt.subplots(figsize=(12, 6))
+    fig, ax = plt.subplots(figsize=(12, 6)) # 創建一個大小為 12x6 英寸的圖形，並返回圖形 (fig) 和坐標軸 (ax) 物件
     ax.plot(df.index, df['value'], color='tab:blue')
     ax.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
     ax.set_xlabel('Date')
     ax.set_ylabel('Page Views')
     plt.xticks(rotation=45)
-    plt.tight_layout()
+    plt.tight_layout() # 自動調整子圖的間距，以防止標籤或標題重疊
 
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
@@ -30,9 +31,9 @@ def draw_bar_plot():
     # Copy and modify data for monthly bar plot
     df_bar = df.copy()
     df_bar.reset_index(inplace=True)
-    df_bar['year'] = [d.year for d in df_bar.date]
-    df_bar['month'] = [d.strftime('%b') for d in df_bar.date]
-    df_monthly_avg = df_bar.groupby(['year', 'month'])['value'].mean().unstack()
+    df_bar['year'] = [d.year for d in df_bar.date] # date 列轉換為年份（d.year）
+    df_bar['month'] = [d.strftime('%b') for d in df_bar.date] # 轉換成月份
+    df_monthly_avg = df_bar.groupby(['year', 'month'])['value'].mean().unstack() # 計算每組的平均值，並使用 unstack() 轉換為以年份為列索引、月份為行索引的 DataFrame
     # Draw bar plot
 
     fig, ax = plt.subplots(figsize=(12, 6))
